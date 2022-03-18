5d4c0ecc12e0efec76af693c884ef5120f52e400
Lenkors <a.vahitov@gmail.com>
2022-03-19 03:41:43 +0600

    added ts

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,28 @@ import router from './router'
 import store from './store'
 import VueFinalModal from 'vue-final-modal'
 import lwInput from './components/ui/lw-input.vue'
-
+import svgBase from './components/ui/svgBase.vue'
 // Style
 import './assets/styles/vendor.scss'
 
 const app = createApp(App)
+
+// Packages
+
 app.use(VueFinalModal(), { 
     componentName: 'VueFinalModal',
     key: '$vfm',    
     dynamicContainerName: 'ModalsContainer'
 })
+
+
+// Components
+
 app.component('lw-input', lwInput)
+app.component('svg-base', svgBase)
+
+
 app.use(router)
 app.use(store)
 app.use(vuetify)
-
 app.mount('#app') 
